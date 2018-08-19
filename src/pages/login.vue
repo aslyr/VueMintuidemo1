74aebb9c788de613c57288cbb92111b085fd59d0
aslyr <380234169@qq.com>
2018-08-19 23:57:55 +0800

    export 导出

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,8 +1,21 @@
 <template>
-    <div><h1>这是使用vue组件定义出来的组件</h1></div>
+    <div><h1 >这是使用vue组件定义出来的组件{{name}}</h1><button @click="show" >确定</button></div>
+    
 </template>
 <script>
-
+export default{
+    data:function(){
+        return{
+            msg:123,
+            name:"高凯"
+        }
+    },
+    methods:{
+        show(){
+            console.log(this.msg+this.name)
+        }
+    }
+}
 </script>
 <style>
 </style>
