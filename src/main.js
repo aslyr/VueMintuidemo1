2fd1fe787abb2829d80251e5989919c7c3744357
aslyr <380234169@qq.com>
2018-08-20 15:11:05 +0800

    初始化mint项目首页

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
+import "mint-ui/lib/style.css"
 import "./lib/mui/css/mui.min.css"
 import Vue from "vue"
+import {Header} from "mint-ui"
 import VueRouter from "vue-router"
 import app from "./pages/app.vue"
 import router1 from "./router.js"
-
+Vue.component(Header.name,Header)
 Vue.use(VueRouter)
 let vm=new Vue(
     {
       el:"#app",
-      data:{
-          msg:"vue，你好！"
-      }  ,
       router:router1,
       render:c=>c(app),
       
--- src/pages/app.vue
+++ src/pages/app.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>
-    <h1>这是使用vue组件定义出来的app组件</h1>
-   
+  <div class="appcontent">
+     <!--header  -->
+     <mt-header fixed title="固定在顶部"></mt-header>
+     <!-- content -->
+     <h1>你好，vue.js</h1>
+     <router-view></router-view>
+     <!-- footer -->
+     <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
   </div>
 
 </template>
@@ -9,6 +31,8 @@
   
 </script>
 <style lang="scss" scoped>
-
+.appcontent{
+  padding-top: 40px;
+}
 </style>
 
